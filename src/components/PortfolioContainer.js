c9704557026c45499b601ac4c15419f12b921738
Rachel <racheljhumes@gmail.com>
2022-11-30 11:29:07 -0700

    commit tree 7a26ab3725e4f9f4429acddbaae5a8549d44f51f

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ stocks, onStockRemove }) {
+  const allStocks = stocks.map((stock) => (
+    <Stock key={stock.id} stock={stock} onStockClick={onStockRemove} />
+  ));
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {allStocks}
     </div>
   );
 }
